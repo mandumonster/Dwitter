c48b3b5ccb597628e5ba6a00d1ea91eb93b61cf1
mzgz <minjugim639@gmail.com>
2023-11-03 14:23:46 +0900

    과제 틀

---
+++ data/auth.js
@@ -0,0 +1,10 @@
+let users=[
+    {
+        id:'1',
+        username:'apple',
+        password:'$2b$10$6NVVL4gEtPh684Ncn2sCRe/LPe0u4kRkhBYSoiLx4bTGW5gwQ58Dy',
+        name:'김사과',
+        email:'apple@apple.com',
+        url:'https://i.pinimg.com/originals/a8/dc/63/a8dc63c8abeeb6708dbec6ef3009608a.jpg'
+    }
+];
---
+++ router/auth.js
@@ -0,0 +1,13 @@
+/*
+회원가입
+router.post('/signup',...)  create
+
+로그인
+router.post('/login;,...)
+id와 password를 보내준다 포스트로 제이슨으로
+
+로그인까지 하고 깃허브에 올리기
+
+JWT 확인
+router.get('/me',...)
+*/
