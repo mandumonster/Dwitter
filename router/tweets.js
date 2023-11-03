6f70475cece25be82a984fde2d3588b2093a1d93
mzgz <minjugim639@gmail.com>
2023-11-03 11:14:41 +0900

    validation 적용

--- router/tweets.js
+++ router/tweets.js
@@ -1,8 +1,18 @@
 import express from "express";
 import * as tweetController from '../controller/tweet.js';
+import {validate} from '../middleware/validator.js'
+import {body} from 'express-validator';
 
 const router=express.Router();
 
+const validateTweet=[
+    body('text').trim().isLength({min:3}).withMessage('최소 3자 이상 입력하세요'),validate
+];
+
+
+/*
+post, put의 text에 빈문자열을 없애고, 최소 3자 이상 입력해야 저장되도록 API에 적용
+*/
 
 // GET / tweets
 // GET / tweets?username=:username
@@ -15,10 +25,10 @@ router.get('/',tweetController.getTweets);
 router.get('/:id',tweetController.getTweet);
 
 // POST / tweets
-router.post('/',tweetController.createTweet);
+router.post('/',validateTweet,tweetController.createTweet);
 
 // PUT / tweets/:id
-router.put('/:id',tweetController.updateTweet);
+router.put('/:id',validateTweet,tweetController.updateTweet);
 
 // DELETE / tweets/:id
 router.delete('/:id',tweetController.deleteTweet);
